1a96de2bf85b02f8342ef7474ad9324badba651d
plilov <petkolilov@icloud.com>
2024-08-05 20:08:06 -0500

    adding files

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -2,6 +2,8 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println("Project Git");
         System.out.println("New changes");
+        System.out.println("stash");
+
 
     }
 }
